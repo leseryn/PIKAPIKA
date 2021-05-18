e9a2d21316e00f332b76d6f2558c50ce8cadc8dd
leseryn <yuisi2008@gmail.com>
2021-05-18 16:24:46 +0800

    v1.0.4

--- conf.py
+++ conf.py
@@ -30,6 +30,7 @@ release = 'v1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+pip install myst_parser
 extensions = [
     'myst_parser'
 ]
